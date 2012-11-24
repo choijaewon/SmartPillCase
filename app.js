8bd4e4d9c8b8783b7a0706c15bc43b293ae55b2e
HyunsukLee <hyunsuk76@gmail.com>
2012-11-25 01:38:03 +0900

    app.js 주석제거

--- app.js
+++ app.js
@@ -10,28 +10,19 @@ Ext.application({
 
     requires: [
         'Ext.MessageBox',
-<<<<<<< HEAD
         'Ext.data.Store',
-        'Ext.data.Model'
-=======
+        'Ext.data.Model',
         'Ext.picker.Picker'
->>>>>>> 등록페이지 수정
     ],
     
     controllers : ['InsertFormController'],
 
-<<<<<<< HEAD
     views: ['Main','List','Home','Setup'],
-<<<<<<< HEAD
     models      : ['MyPhoneNumber','MyPill'],
     stores      : ['PhoneNumber','Pill'],
-=======
     models      : ['MyPhoneNumber'],
     stores      : ['PhoneNumber'],
-=======
     views: ['Main','List','Home','Setup','InsertForm','Yoil'],
->>>>>>> 등록페이지 수정
->>>>>>> 66cfca05a1a7123d55f2a2ed5483b04aba91f1ec
 
     icon: {
         '57': 'resources/icons/Icon.png',
@@ -56,15 +47,12 @@ Ext.application({
         Ext.fly('appLoadingIndicator').destroy();
 
         // Initialize the main view
-<<<<<<< HEAD
         Ext.Viewport.add(Ext.create('SmartPillCase.view.Main'));   
         		
         
-=======
 //        Ext.Viewport.add(Ext.create('SmartPillCase.view.Main'));
         Ext.Viewport.add(Ext.create('SmartPillCase.view.InsertForm'));
 //        Ext.Viewport.add(Ext.create('SmartPillCase.view.Yoil'));
->>>>>>> 등록페이지 수정
     },
 
     onUpdated: function() {
